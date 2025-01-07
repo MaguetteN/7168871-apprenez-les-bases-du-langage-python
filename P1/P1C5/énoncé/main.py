2fb5f1f81ebcf5fbac803fa0cb56b8f4ced5af22
MaguetteN <151390375+MaguetteN@users.noreply.github.com>
2025-01-07 15:22:58 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
-# Écrivez votre code ici !
+nom= "ndiaye"
+prenom=" ndeye maguette"
+age=22
+taille=1,75
+est_etudiant=True
+print(f"nom: {nom}")
+print(f"age: {age}")
+print(f"taille: {taille}")
+print(f"est_etudiant: {est_etudiant}")
+print(f"le types est: {type(nom)}")
+print(f"le types est: {type(age)}")
+print(f"le types est: {type(taille)}")
+print(f"le types est: {type(est_etudiant)}")
+
