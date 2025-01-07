965fbb6e8ef04ad7cca8bae2841f09ee83c06799
MaguetteN <151390375+MaguetteN@users.noreply.github.com>
2025-01-07 14:44:53 +0000

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+nom= "ndiaye"
+prenom=" ndeye maguette"
+age=22
+print(f"Bonjour je m'appelle {prenom} {nom} et j'ai {age} ans")
+age=32
+print(f"Bonjour je m'appelle {prenom} {nom} et j'ai {age} ans")
